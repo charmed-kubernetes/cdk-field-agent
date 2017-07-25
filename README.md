ea71a9daba34746ad2dbd9a55df339bffb9ac050
Rye Terrell <ryeterrell@ryeterrell.net>
2017-07-25 13:59:16 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,36 @@
+# CDK Field Agent
+
+Collects data from a CDK deployment for HQ to analyze.
+
+### Quick start
+
+ - Download and execute the `collect.py` script from this repo on a box that has
+Juju configured such that the current controller and model are pointed at your
+CDK deployment of interest.
+
+ - Wait a little bit.
+
+ - Send the resulting .tar.gz file back to HQ for analysis.
+
+ - Standby for instruction.
+
+### Manual steps
+
+The purpose of CDK Field Agent is to save you the tedium of collecting the data
+we may need to debug your problems in production. If for some reason things don't
+proceed smoothly, here's the data collected. Please collect it and send it to us:
+
+ - debug action output from all kubernetes-master units
+ - debug action output from all kubernetes-worker units
+ - debug action output from all etcd units
+ - `juju status --format yaml`
+ - `juju debug-log --replay`
+ - `juju storage --format yaml`
+ - `juju storage-pools --format yaml`
+ - `juju config kubernetes-master --format yaml`
+ - `juju config kubernetes-worker --format yaml`
+ - `juju config kubeapi-load-balancer --format yaml`
+ - `juju config etcd --format yaml`
+ - `juju config easyrsa --format yaml`
+ - `juju config flannel --format yaml`
+ 
---
+++ collect.py
@@ -0,0 +1,92 @@
+#!/usr/bin/env python3
+
+import json
+import os
+import tempfile
+import time
+
+from subprocess import check_output, check_call, CalledProcessError, Popen
+
+
+def debug_action(temppath, status, application):
+    # FIXME: doesn't handle subordinate apps properly yet
+    # FIXME: these could be done in parallel
+    apps = status.get('applications', {})
+    app = apps.get(application, {})
+    units = app.get('units', {})
+    for unit in list(units.keys()):
+        cmd = 'juju run-action %s debug --format json' % unit
+        raw_action = check_output(cmd.split())
+        action = json.loads(raw_action.decode())
+        action_id = action['Action queued with id']
+        print('Executing debug action on %s...' % unit)
+        while True:
+            # FIXME: blocks forever in a couple cases
+            cmd = 'juju show-action-output %s --format json' % action_id
+            raw_action_output = check_output(cmd.split())
+            action_output = json.loads(raw_action_output.decode())
+            if action_output['status'] in ['running', 'pending']:
+                time.sleep(1)
+                continue
+            if action_output['status'] == 'completed':
+                unit_name = unit.split('/')[0]
+                unit_id = unit.split('/')[1]
+                outpath = os.path.join(temppath, 'debug', unit_name, unit_id)
+                os.makedirs(outpath)
+                cmd = 'juju scp %s:%s %s' % (unit, action_output['results']['path'], outpath)
+                check_call(cmd.split())
+                break
+            print('Failed debug action on unit %s, status %s' % (unit, action_output['status']))
+            break
+
+
+def command(temppath, filename, cmd):
+    print('Running %s...' % cmd)
+    path = os.path.join(temppath, filename)
+    stdout = open(path+'.out', 'w')
+    stderr = open(path+'.err', 'w')
+    proc = Popen(cmd.split(), stdout=stdout, stderr=stderr)
+    proc.wait()
+    stdout.close()
+    stderr.close()
+
+
+def store_results(temppath):
+    ts = time.asctime(time.gmtime()).replace(' ', '-').replace(':', '-')
+    fname = 'results-%s.tar.gz' % ts
+    cmd = 'tar -C %s -czf %s results' % (os.path.dirname(temppath), fname)
+    check_call(cmd.split())
+    print('Results stored in %s.' % fname)
+
+
+def main():
+    tempdir = tempfile.TemporaryDirectory()
+    temppath = os.path.join(tempdir.name, 'results')
+    os.makedirs(temppath)
+
+    print('Getting juju status...')
+    raw_status = check_output('juju status --format json'.split())
+    status = json.loads(raw_status.decode())
+    apps = status.get('applications', {})
+
+    debug_action(temppath, status, 'kubernetes-master')
+    debug_action(temppath, status, 'kubernetes-worker')
+    debug_action(temppath, status, 'etcd')
+    # FIXME: no debug action on kubeapi-load-balancer, easyrsa, flannel
+
+    command(temppath, 'status', 'juju status --format yaml')
+    command(temppath, 'debug-log', 'juju debug-log --replay')
+    command(temppath, 'storage', 'juju storage --format yaml')
+    command(temppath, 'storage-pools', 'juju storage-pools --format yaml')
+    command(temppath, 'kubernetes-master-config', 'juju config kubernetes-master --format yaml')
+    command(temppath, 'kubernetes-worker-config', 'juju config kubernetes-worker --format yaml')
+    command(temppath, 'kubeapi-load-balancer-config', 'juju config kubeapi-load-balancer --format yaml')
+    command(temppath, 'etcd-config', 'juju config etcd --format yaml')
+    command(temppath, 'easyrsa-config', 'juju config easyrsa --format yaml')
+    command(temppath, 'flannel-config', 'juju config flannel --format yaml')
+
+    store_results(temppath)
+
+
+if __name__ == '__main__':
+    main()
